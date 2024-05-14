4bddb627440fddb3677eb56f33464bcd2bab7f4f
GaziFayaz <gazifayaz.16694@gmail.com>
2024-05-14 16:58:21 +0600

    my bookings page partial

--- src/Components/Bookings/MyBookings.jsx
+++ src/Components/Bookings/MyBookings.jsx
@@ -1,22 +1,59 @@
-import React, { useContext, useEffect, useState } from 'react'
-import { Helmet } from 'react-helmet-async';
-import { AuthContext } from '../../Providers/AuthProvider';
-import axios from 'axios';
+import React, { useContext, useEffect, useState } from "react";
+import { Helmet } from "react-helmet-async";
+import { AuthContext } from "../../Providers/AuthProvider";
+import axios from "axios";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { FaCheck } from "react-icons/fa";
+import { IoCloseSharp } from "react-icons/io5";
 
 const MyBookings = () => {
-  const [loading, setLoading] = useState(true)
-  const [bookings, setBookings] = useState([])
-  const {user} = useContext(AuthContext)
-  useEffect(() => {
-    axios.get(`${import.meta.env.VITE_SERVER_URL}/user-bookings/${user.uid}`)
-    .then(res => {
-      // console.log(res.data)
-      setBookings(res.data)
-      setLoading(false)
-    })
-  }, [user.uid])
-  return (
-    <div className=" flex flex-col mb-20">
+	const [loading, setLoading] = useState(true);
+	const [bookings, setBookings] = useState([]);
+	const [updateClicked, setUpdateClicked] = useState(false);
+	const [updateClickedFor, setUpdateClickedFor] = useState(null);
+	const [selectedDate, setSelectedDate] = useState(null);
+	const { user } = useContext(AuthContext);
+	useEffect(() => {
+		axios
+			.get(`${import.meta.env.VITE_SERVER_URL}/user-bookings/${user.uid}`)
+			.then((res) => {
+				console.log(res.data);
+				setBookings(res.data);
+				setLoading(false);
+			});
+	}, [user.uid]);
+
+	const handleUpdate = (_id) => {
+		console.log("booking id", _id);
+		console.log("sending Date", selectedDate, typeof selectedDate);
+		axios.post(
+			`${import.meta.env.VITE_SERVER_URL}/update-booking-date/${_id}`,
+			{ date: selectedDate }
+		);
+		setUpdateClickedFor(null);
+		setSelectedDate(null);
+		setUpdateClicked(false);
+	};
+
+	const handleDelete = (bookingId, room) => {
+		axios.post(`${import.meta.env.VITE_SERVER_URL}/user-cancel-book/${user.uid}`, {bookingId: bookingId})
+		.then( () => {
+			axios.delete(`${import.meta.env.VITE_SERVER_URL}/delete-book/${bookingId}`)
+			.then( () => {
+				axios.post(`${import.meta.env.VITE_SERVER_URL}/unbook-room/${room}`)
+				setBookings(bookings.forEach( booking => {
+					if(booking._id !== bookingId){
+						return booking
+					}
+				}))
+			})
+		})
+		
+	}
+
+	return (
+		<div className=" flex flex-col mb-20">
 			<Helmet>
 				<title>Majestic Oasis | My Bookings</title>
 			</Helmet>
@@ -25,94 +62,101 @@ const MyBookings = () => {
 					<span className="loading loading-infinity loading-lg"></span>
 				</div>
 			) : (
-				<div>
+				<div className="flex flex-col items-center justify-center">
 					<div
-						className={`bg-gray-900 min-w-screen  py-20 md:py-0   md:min-h-48 lg:min-h-64 bg-cover bg-center bg-no-repeat flex flex-col gap-10 items-center justify-center text-white font-cinzel`}
+						className={`w-full bg-gray-900 min-w-screen  py-20 md:py-0   md:min-h-48 lg:min-h-64 bg-cover bg-center bg-no-repeat flex flex-col gap-10 items-center justify-center text-white font-cinzel`}
 					>
 						<h1 className="text-amber-400 text-2xl md:text-4xl lg:text-5xl text-center">
-							{room.category}
+							My Bookings
 						</h1>
 					</div>
-					<div className="mx-6 md:mx-16 lg:mx-24">
-						<div className="mt-8 lg:mt-8 card lg:card-side bg-base-100 shadow-xl">
-							<figure className="lg:w-1/2">
-								<Swiper
-									className=""
-									pagination={{
-										dynamicBullets: true,
-									}}
-									loop={true}
-									autoplay={{
-										delay: 5000,
-										disableOnInteraction: false,
-									}}
-									modules={[Autoplay, Pagination]}
-								>
-									{room.room_images.map((image, index) => {
+					<div className="w-full px-6 md:px-16 lg:px-24">
+						<div className="overflow-x-auto w-full">
+							<table className="table-lg w-full">
+								{/* head */}
+								<thead>
+									<tr>
+										<th></th>
+										<th>Date</th>
+										<th>Room</th>
+										<th>Price</th>
+										<th></th>
+									</tr>
+								</thead>
+								<tbody>
+									{bookings.map((booking) => {
 										return (
-											<SwiperSlide key={index}>
-												<img
-													className="object-cover h-full w-full"
-													src={image}
-												/>
-											</SwiperSlide>
+											<tr key={booking._id}>
+												<th>1</th>
+												<td className="">
+													{new Date(booking.date).toLocaleDateString()}
+												</td>
+												<td>{booking.room}</td>
+												<td>{booking.price_per_night}</td>
+												<td>
+													<button className="btn bg-amber-500 text-white ">
+														Review
+													</button>
+												</td>
+												<td>
+													{updateClicked && updateClickedFor === booking._id ? (
+														<div className="flex items-center gap-2">
+															{console.log(
+																"type of date from booking",
+																typeof booking.date
+															)}
+															<DatePicker
+																className="border-2 border-black p-2 rounded-md"
+																selected={
+																	selectedDate
+																		? selectedDate
+																		: new Date(booking.date)
+																}
+																onChange={(date) => {
+																	setSelectedDate(date);
+																	console.log(
+																		"type of date from onchange",
+																		typeof date
+																	);
+																}}
+															/>
+															<FaCheck
+																onClick={() => handleUpdate(booking._id)}
+																className="text-xl cursor-pointer"
+															/>
+															<IoCloseSharp onClick={() => {
+																setUpdateClicked(false)
+																setUpdateClickedFor(null)
+															}} className="text-3xl cursor-pointer" />
+														</div>
+													) : (
+														<button
+															onClick={() => {
+																setUpdateClicked(!updateClicked);
+																setUpdateClickedFor(booking._id);
+															}}
+															className="btn btn-success text-white"
+														>
+															Update
+														</button>
+													)}
+												</td>
+												<td>
+													<button onClick={() => handleDelete(booking._id, booking.room)} className="btn btn-error text-white">
+														Cancel
+													</button>
+												</td>
+											</tr>
 										);
 									})}
-								</Swiper>
-							</figure>
-							<div className="card-body lg:p-16 lg:space-y-5">
-								<h1 className="text-2xl md:text-3xl font-lato font-bold">
-									{room.category}
-								</h1>
-								<p className="font-lato text-xl">{room.description}</p>
-								<p className="text-xl font-lato">
-									<span className="font-bold ">Price:</span> $
-									{room.price_per_night}
-								</p>
-								<p className="text-xl font-lato">
-									<span className="font-bold ">Room Size:</span>{" "}
-									{room.room_size} sq ft
-								</p>
-								{room.special_offers ? (
-									<p className="text-xl font-lato">
-										<span className="font-bold ">Special Offer: </span>{" "}
-										{room.special_offers}
-									</p>
-								) : (
-									<></>
-								)}
-
-								<div className="card-actions justify-end">
-									{room.availability === "Available" ? (
-										<div className="flex items-center lg: gap-4">
-											<DatePicker
-												className="border-2 border-black p-2 rounded-md"
-												selected={startDate}
-												onChange={(date) => {
-													setStartDate(date);
-													console.log(date);
-												}}
-											/>
-											<button
-												onClick={handleBook}
-												className="btn bg-black text-amber-400 font-lato text-lg"
-											>
-												Book Now
-											</button>
-										</div>
-									) : (
-										<p className="card-actions justify-end text-xl font-bold font-lato text-red-600">
-											Room Unavailable
-										</p>
-									)}
-								</div>
-							</div>
+								</tbody>
+							</table>
 						</div>
 					</div>
 				</div>
 			)}
 		</div>
-  )
-}
+	);
+};
 
-export default MyBookings
+export default MyBookings;
